023eba8ae18bfcbf967854abb14fb8706c5659ef
Andy Britcliffe <andybritcliffe@quillcontent.com>
2016-04-19 21:39:43 +0100

    added static file serving.

--- main.go
+++ main.go
@@ -3,19 +3,23 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
-  port := ":8080"
-  http.HandleFunc("/", RootHandler)
-  fmt.Printf("### Server started on %s ###", port)
-  http.ListenAndServe(port, nil)
+	port := ":8080"
+	fs := http.FileServer(http.Dir("dist"))
+	http.Handle("/dist/", http.StripPrefix("/dist/", fs))
+	http.HandleFunc("/", RootHandler)
+	log.Printf("### Server started on %s ###", port)
+	http.ListenAndServe(port, nil)
 }
 
 func RootHandler(rw http.ResponseWriter, req *http.Request) {
 	t, err := template.ParseFiles("index.html")
 	if err != nil {
+		rw.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(rw, "Error: %s", err)
 	}
 
